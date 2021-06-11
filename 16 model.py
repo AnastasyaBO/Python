ccddff97040b50793e791b38ee7a5d891d022540
AnastasyaBO <bae.testingqa24@gmail.com>
2021-06-11 12:40:58 +0300

    "New File"

---
+++ 16 model.py
@@ -0,0 +1,14 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def getWidth(self):
+        return self.width
+
+    def getHeight(self):
+        return self.height
+
+    def getArea(self):
+        return self.width * self.height
+
